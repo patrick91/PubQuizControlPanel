0e651f9dbbbd321ea447cc7d9abd241bcf9d4c89
Marco Acierno <marcoacierno96@gmail.com>
2016-05-09 00:53:06 +0200

    Initial Firebase integration

--- js/pages/edit-game/index.js
+++ js/pages/edit-game/index.js
@@ -1,76 +1,14 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 
-import { Editor, EditorState, ContentState } from 'draft-js';
+import { Editor, EditorState, ContentState, convertToRaw } from 'draft-js';
 
 import PlayIcon from '../../components/play-icon';
 import classNames from 'classnames';
 
+import Firebase from 'firebase';
+
 import styles from './styles.css';
-
-
-const questions = [
-    {
-        title: 'Question 1',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-    {
-        title: 'Question 2',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-    {
-        title: 'Question 3',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-    {
-        title: 'Question 4',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-    {
-        title: 'Question 5',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-    {
-        title: 'Question 6',
-        correct: 1,
-        answers: [
-            'option 1',
-            'option 2',
-            'option 3',
-            'option 4',
-        ],
-    },
-]
 
 
 class EditGame extends React.Component {
@@ -81,21 +19,60 @@ class EditGame extends React.Component {
     constructor() {
         super();
 
-        const editors = [];
-
-        for (const question of questions) {
-            editors.push(this.createQuestion(question));
-        }
-
         this.state = {
             code: '',
-            editors,
+            editors: [],
         };
 
         this.renderQuestion = this.renderQuestion.bind(this);
         this.onChangeQuestion = this.onChangeQuestion.bind(this);
         this.onChangeAnswer = this.onChangeAnswer.bind(this);
         this.addNewQuestion = this.addNewQuestion.bind(this);
+        this.onGameObjectUpdate = this.onGameObjectUpdate.bind(this);
+    }
+
+    componentWillMount() {
+        // we can replace on with once if we don't want to allow multiple users
+        // to edit the same game at the same time
+        // we might be more clever and allow multiple users by using child_added, child_changed
+        // etc.
+        const gameCode = this.props.params.code;
+
+        this.backend = new Firebase(`https://testquizapp123456.firebaseio.com/games/${gameCode}`);
+        this.backend.on('value', this.onGameObjectUpdate);
+    }
+
+    componentWillUnmount() {
+        this.backend.off('value', this.onGameObjectUpdate);
+    }
+
+    onGameObjectUpdate(snapshot) {
+        if (snapshot.val() === null) {
+            // TODO:
+            // invalid game code
+            console.log('invalid game code');
+            return;
+        }
+
+        const game = snapshot.val();
+
+        this.initializeUi(game);
+        this.setState({
+            game,
+        });
+    }
+
+    initializeUi(game) {
+        console.log('game', game);
+        const editors = [];
+
+        game.questions.forEach(question => {
+            editors.push(this.createQuestion(question));
+        });
+
+        this.setState({
+            editors,
+        });
     }
 
     onChangeQuestion(index, editorState) {
@@ -103,35 +80,51 @@ class EditGame extends React.Component {
 
         editors[index].title = editorState;
 
-        this.setState({editors});
+        const textBlocks = convertToRaw(editorState.getCurrentContent()).blocks;
+        const text = textBlocks.reduce((previousValue, block, _) => {
+            return `${previousValue} ${block.text}`;
+        }, '');
+
+        this.backend.child(`questions/${index}/title`).set(text);
+        this.setState({ editors });
     }
 
     onChangeAnswer(question, index, editorState) {
         const editors = [...this.state.editors];
 
+        const textBlocks = convertToRaw(editorState.getCurrentContent()).blocks;
+        const text = textBlocks.reduce((previousValue, block, _) => {
+            return `${previousValue} ${block.text}`;
+        }, '');
+
+        this.backend.child(`questions/${question}/answers/${index}`).set(text);
         editors[question].answers[index] = editorState;
 
-        this.setState({editors});
+        this.setState({ editors });
     }
 
     addNewQuestion() {
         const question = {
             title: 'random question',
-            answers: [],
+            answers: ['1', '2', '3', '4'],
             correct: 0,
         };
 
-        questions.push(question);
+        this.backend.child('questions').transaction(currentQuestions => {
+            return [
+                ...currentQuestions,
+                question,
+            ];
+        });
 
         const editors = [...this.state.editors];
 
         editors.push(this.createQuestion(question));
 
-        this.setState({editors}, () => {
+        this.setState({ editors }, () => {
             window.scrollTo(0, document.body.scrollHeight);
         });
     }
-
 
     createQuestion(question) {
         const content = ContentState.createFromText(question.title);
@@ -194,9 +187,17 @@ class EditGame extends React.Component {
             <ul>
                 { this.state.editors.map(this.renderQuestion) }
 
-                <li styleName='add-new-question' onClick={this.addNewQuestion}>Add new question</li>
+                {!this.isGameStarted() && <li styleName='add-new-question' onClick={this.addNewQuestion}>Add new question</li>}
             </ul>
         </div>;
+    }
+
+    isGameFinished() {
+        return this.state.game.status === 'finished';
+    }
+
+    isGameStarted() {
+        return this.state.game.status === 'running';
     }
 }
 
